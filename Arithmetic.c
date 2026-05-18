8fc382ad88fbd58aa0fc6b29ba8853061aa6b2e0
Tolorunlogo Darasimi <darasimitolorunlogo@gmail.com>
2026-05-18 13:02:02 +0100

    - Arithmetic operation. - Print a error output if divided by zero. - Change the functions and it's parameter type to double. - collect input from user to choose the operation they want. - Create if statement to calculate and print the result using the operation the user will choose.

--- Arithmetic.c
+++ Arithmetic.c
@@ -1,44 +1,63 @@
 #include <stdio.h>
 
-int add(int a, int b)
+double add(double a, double b)
 {
     return a += b;
 }
 
-int multiply(int a, int b)
+double multiply(double a, double b)
 {
     return a *= b;
 }
 
-int subtract(int a, int b)
+double subtract(double a, double b)
 {
     return a -= b;
 }
 
-float divide(int a, int b)
+double divide(double a, double b)
 {
-    float x = (float)a;
-    float y = (float)b;
-    return x /= y;
+    if(b == 0){
+        printf("Error! Division by zero\n");
+        return 0;
+    }else{
+        return a /= b;
+    }
+
 }
 
 int main()
 {
-    int num1, num2;
-    int addition, subtraction, multiplication;
-    float division;
+    double num1, num2, result;
+    int choice;
 
-    printf("Enter two integer number: ");
-    scanf("%d%d", &num1, &num2);
-    addition = add(num1, num2);
-    multiplication = multiply(num1, num2);
-    subtraction = subtract(num1, num2);
-    division = divide(num1, num2);
+    printf("1. Addition\n");
+    printf("2. Subtraction\n");
+    printf("3. Multiplication\n");
+    printf("4. Division\n");
+    printf("Choose the operation to perform: ");
+    scanf("%d", &choice);
 
-    printf("Addition of %d and %d: %d\n", num1, num2, addition);
-    printf("Subraction of %d and %d: %d\n", num1, num2, subtraction);
-    printf("Multiplication of %d and %d: %d\n", num1, num2, multiplication);
-    printf("Division of %d and %d: %.2f\n", num1, num2, division);
+    printf("Enter the first number: ");
+    scanf("%lf", &num1);
+    printf("Enter the second number: ");
+    scanf("%lf", &num2);
 
+    if(choice == 1){
+        result = add(num1, num2);
+        printf("Result: %.1lf\n", result);
+    }else if(choice == 2){
+        result = subtract(num1, num2);
+        printf("Result: %.1lf\n", result);
+    }else if(choice == 3){
+        result = multiply(num1, num2);
+        printf("Result: %.1lf\n", result);
+    }else if(choice == 4){
+        result = divide(num1, num2);
+        printf("Result: %.2lf\n", result);
+    }else{
+        printf("Invalid choice");
+        return 1;
+    }
     return 0;
 }
